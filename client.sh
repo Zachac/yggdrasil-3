e96b88de032a9bc3facad5b12bce34da52c60f45
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2019-08-26 17:35:40 -0500

    Updating client/server

--- client.sh
+++ client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Start client
-openssl s_client -connect localhost:5557 -quiet -verify_quiet
+openssl s_client -connect localhost:3329 -quiet -verify_quiet
 
--- server.sh
+++ server.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+RUNTIME="$DIR/data/runtime"
+STDOUT="$RUNTIME/stdout"
+STDIN="$RUNTIME/stdin"
 
-echo "Starting server at $DIR"
+source "$DIR/data/settings/server"
+
+echo "Starting server at $DIR on port $SERVER_PORT"
 
 # Cleanup existing runtime directory
 rm -r "$DIR/data/runtime" 2>/dev/null
 
 
-# Setup files
+# Setup files & data
 mkdir "$DIR/data/runtime"
+
+CLIENTS=()
 
 
 # Start server
-openssl s_server -accept 5557 -key data/certs/keyfile.key -cert data/certs/certfile.crt -quiet -verify_quiet
+mkfifo $STDOUT
+mkfifo $STDIN
+
+openssl s_server -accept $SERVER_PORT -key data/certs/keyfile.key -cert data/certs/certfile.crt -naccept 1 -quiet -verify_quiet >$STDOUT <$STDIN &
+
+while read line <$STDOUT; do
+	echo "Line $line"
+	echo "Line $line" >$STDIN
+done
+
+
+
 
