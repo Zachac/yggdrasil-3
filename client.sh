e41a9d37468a30c8fa6073e11c34ed41b154c78a
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2019-08-27 12:43:41 -0500

    Polish up echo server/client

--- client.sh
+++ client.sh
@@ -4,6 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 RUNTIME="$DIR/data/runtime/client"
 STDOUT="$RUNTIME/stdout"
 STDIN="$RUNTIME/stdin"
+STDERR="$RUNTIME/err"
 
 source "$DIR/data/settings/client"
 
@@ -16,19 +17,43 @@ mkdir -p "$RUNTIME"
 # Init file descriptors
 mkfifo $STDIN
 mkfifo $STDOUT
+mkfifo $STDERR
 
 exec 3<>$STDIN
 exec 4<>$STDOUT
+exec 5<>$STDERR
 
-# Start client
-(
-	cat <&3 | openssl s_client -connect "$SERVER_HOST:$SERVER_PORT" -quiet -verify_quiet
+
+( # Start client
+	echo "Starting client to $SERVER_HOST:$SERVER_PORT"
+	cat <&3 | openssl s_client -connect "$SERVER_HOST:$SERVER_PORT" -quiet -verify_quiet >&4 2>&5
 	echo "Disconnected"
 	kill $PID
+) &
+
+
+( # Filter output
+	while read line <&4; do
+		if [ "${line:0:1}" = ">" ]; then
+			echo "${line:1}"
+		else
+			echo "unkown input: $line"
+		fi
+	done
+) &
+
+( # Wait for error signal from client
+	while read line <&5; do
+		if grep -q '^[a-zA-Z][a-zA-Z]*:errno=[0-9][0-9]*$' <<<$line; then
+			echo "Disconnected"
+			kill $PID
+		fi
+	done
 ) &
 
 # Preprocess input before feeding to server
 while read line; do
 	echo ">$line" >&3
 done
+
 
--- server.sh
+++ server.sh
@@ -4,6 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 RUNTIME="$DIR/data/runtime/server"
 STDOUT="$RUNTIME/stdout"
 STDIN="$RUNTIME/stdin"
+FILTER="$RUNTIME/filter"
 
 source "$DIR/data/settings/server"
 
@@ -19,30 +20,33 @@ mkdir -p "$RUNTIME"
 # Init file descriptors
 mkfifo $STDIN
 mkfifo $STDOUT
+mkfifo $FILTER
 
 exec 3<>$STDIN
 exec 4<>$STDOUT
+exec 5<>$FILTER
 
 
-# Server
-(
+( # Start server
 	echo "Starting server at $DIR on port $SERVER_PORT"
 	cat <&3 | openssl s_server -accept $SERVER_PORT -key $KEY_FILE -cert $CERT_FILE -naccept 1 -verify_quiet >&4
 	echo "Server finished"
-	kill $PID
 ) &
 
-
+( # Encode output
+	while read rawLine <&5; do
+		echo ">$rawLine" >&3
+	done
+) &
 
 # Handle input
 while read line <&4; do
-	# only read lines starting with >
+	# only accept lines starting with >
 	if [ ${line:0:1} = '>' ]; then
 		echo "Accepting ${line:1}"
-		echo "${line:1}" >&3
+		echo "${line:1}" >&5
 	elif [ "$line" = "CONNECTION CLOSED" ]; then
-		# if the connection is closed, flush the output so the server can close
-		echo "" >&3
+		break
 	fi
 done
 
