94743d0dc6b934cf40caebddfa6f4bca865dc745
Zachary Chandler <zachary.a.chandler96@gmail.com>
2019-08-26 20:10:49 -0500

    Add basic echo server

--- client.sh
+++ client.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+RUNTIME="$DIR/data/runtime/client"
+STDOUT="$RUNTIME/stdout"
+STDIN="$RUNTIME/stdin"
+
+source "$DIR/data/settings/client"
+
+PID=$$
+
+# Cleanup existing runtime directory
+rm -r "$RUNTIME" 2>/dev/null
+mkdir -p "$RUNTIME"
+
+# Init file descriptors
+mkfifo $STDIN
+mkfifo $STDOUT
+
+exec 3<>$STDIN
+exec 4<>$STDOUT
+
 # Start client
-openssl s_client -connect localhost:3329 -quiet -verify_quiet
+(
+	cat <&3 | openssl s_client -connect "$SERVER_HOST:$SERVER_PORT" -quiet -verify_quiet
+	echo "Disconnected"
+	kill $PID
+) &
+
+# Preprocess input before feeding to server
+while read line; do
+	echo ">$line" >&3
+done
 
--- server.sh
+++ server.sh
@@ -1,35 +1,48 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-RUNTIME="$DIR/data/runtime"
+RUNTIME="$DIR/data/runtime/server"
 STDOUT="$RUNTIME/stdout"
 STDIN="$RUNTIME/stdin"
 
 source "$DIR/data/settings/server"
 
-echo "Starting server at $DIR on port $SERVER_PORT"
+KEY_FILE="$DIR/$SSL_KEY"
+CERT_FILE="$DIR/$SSL_CERT"
+CLIENTS=()
+PID=$$
 
 # Cleanup existing runtime directory
-rm -r "$DIR/data/runtime" 2>/dev/null
+rm -r "$RUNTIME" 2>/dev/null
+mkdir -p "$RUNTIME"
 
-
-# Setup files & data
-mkdir "$DIR/data/runtime"
-
-CLIENTS=()
-
-
-# Start server
-mkfifo $STDOUT
+# Init file descriptors
 mkfifo $STDIN
+mkfifo $STDOUT
 
-openssl s_server -accept $SERVER_PORT -key data/certs/keyfile.key -cert data/certs/certfile.crt -naccept 1 -quiet -verify_quiet >$STDOUT <$STDIN &
+exec 3<>$STDIN
+exec 4<>$STDOUT
 
-while read line <$STDOUT; do
-	echo "Line $line"
-	echo "Line $line" >$STDIN
+
+# Server
+(
+	echo "Starting server at $DIR on port $SERVER_PORT"
+	cat <&3 | openssl s_server -accept $SERVER_PORT -key $KEY_FILE -cert $CERT_FILE -naccept 1 -verify_quiet >&4
+	echo "Server finished"
+	kill $PID
+) &
+
+
+
+# Handle input
+while read line <&4; do
+	# only read lines starting with >
+	if [ ${line:0:1} = '>' ]; then
+		echo "Accepting ${line:1}"
+		echo "${line:1}" >&3
+	elif [ "$line" = "CONNECTION CLOSED" ]; then
+		# if the connection is closed, flush the output so the server can close
+		echo "" >&3
+	fi
 done
-
-
-
 
