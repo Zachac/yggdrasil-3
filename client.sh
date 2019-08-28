297e957d115d74c4a61d4ad69b838cfdc572fa00
Zachary Chandler <zachary.a.chandler96@gmail.com>
2019-08-27 20:26:31 -0500

    refactor client/server

--- client.sh
+++ client.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "$DIR/data/settings/client"
+SERVER_PORT=${2:-$SERVER_PORT}
+SERVER_HOST=${1:-$SERVER_HOST}
+SERVER="$SERVER_HOST:$SERVER_PORT"
 RUNTIME="$DIR/data/runtime/client"
 STDOUT="$RUNTIME/stdout"
 STDIN="$RUNTIME/stdin"
@@ -9,7 +13,7 @@ STDERR="$RUNTIME/err"
 source "$DIR/data/settings/client"
 
 PID=$$
-PGID=$(ps -o pgid= $PID | grep -o [0-9]*)
+PGID=$(ps -o pgid= $PID | grep -o '[0-9]*')
 
 # Cleanup existing runtime directory
 rm -r "$RUNTIME" 2>/dev/null
@@ -26,24 +30,24 @@ exec 5<>$STDERR
 
 
 ( # Start client
-	echo "Starting client to $SERVER_HOST:$SERVER_PORT"
-	cat <&3 | openssl s_client -connect "$SERVER_HOST:$SERVER_PORT" -quiet -verify_quiet >&4 2>&5
+	echo "Starting client to $SERVER"
+	cat <&3 | openssl s_client -connect "$SERVER" -quiet -verify_quiet >&4 2>&5
 	echo "Disconnected"
 	kill -- -$PGID
 ) &
 
 
-( # Filter output
+( # Filter output and send to stdout
 	while read line <&4; do
 		if [ "${line:0:1}" = ">" ]; then
 			echo "${line:1}"
-		else
+		elif ! [ "${line:0:1}" = "#" ]; then
 			echo "unkown input: $line"
 		fi
 	done
 ) &
 
-( # Wait for error signal from client
+( # Wait for error signal from client indicating server has finished
 	while read line <&5; do
 		if grep -q '^[a-zA-Z][a-zA-Z]*:errno=[0-9][0-9]*$' <<<$line; then
 			echo "Disconnected"
@@ -52,9 +56,8 @@ exec 5<>$STDERR
 	done
 ) &
 
-# Preprocess input before feeding to server
+# Preprocess input and then feed to server
 while read line; do
 	echo ">$line" >&3
 done
-
 
--- server.sh
+++ server.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-RUNTIME="$DIR/data/runtime/server"
+source "$DIR/data/settings/server"
+SERVER_PORT=${1:-$SERVER_PORT}
+RUNTIME="$DIR/data/runtime/server/$SERVER_PORT"
 STDOUT="$RUNTIME/stdout"
 STDIN="$RUNTIME/stdin"
 FILTER="$RUNTIME/filter"
 
-source "$DIR/data/settings/server"
-
 KEY_FILE="$DIR/$SSL_KEY"
 CERT_FILE="$DIR/$SSL_CERT"
-CLIENTS=()
 PID=$$
 PGID=$(ps -o pgid= $PID | grep -o [0-9]*)
+
+if ! [ -z "$2" ] && [ "$2" -eq "$2" ]; then
+	NACCEPT="-naccept $2"
+fi
 
 # Cleanup existing runtime directory
 rm -r "$RUNTIME" 2>/dev/null
@@ -30,25 +33,28 @@ exec 5<>$FILTER
 
 ( # Start server
 	echo "Starting server at $DIR on port $SERVER_PORT"
-	cat <&3 | openssl s_server -accept $SERVER_PORT -key $KEY_FILE -cert $CERT_FILE -verify_quiet >&4
+	cat <&3 | openssl s_server -accept $SERVER_PORT -key $KEY_FILE -cert $CERT_FILE -verify_quiet $NACCEPT >&4
 	echo "Server finished"
+	kill -- -$PGID
 ) &
 
-( # Encode output
-	while read rawLine <&5; do
-		echo ">$rawLine" >&3
+( # Handle input
+	while read line <&4; do
+		# only accept lines starting with >
+		if [ ${line:0:1} = '>' ]; then
+			echo "${line:1}"
+		elif [ "$line" = "CONNECTION CLOSED" ]; then
+			echo "#" >&3
+			echo "Client disconnected"
+		fi
 	done
 ) &
 
-# Handle input
-while read line <&4; do
-	# only accept lines starting with >
-	if [ ${line:0:1} = '>' ]; then
-		echo "Accepting ${line:1}"
-		echo "${line:1}" >&5
-	elif [ "$line" = "CONNECTION CLOSED" ]; then
-		echo "Client disconnected"
-	fi
+# Encode output
+while read rawLine; do
+	echo ">$rawLine" >&3
 done
 
+
 kill -- -$PGID
+
