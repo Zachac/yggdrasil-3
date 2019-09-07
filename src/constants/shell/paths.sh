2e082ca5e6403d44bb16425d02890eaf6395432a
Zachary Chandler <zachary.a.chandler96@gmail.com>
2019-09-07 10:56:12 -0500

    add src env variable

--- src/constants/shell/paths.sh
+++ src/constants/shell/paths.sh
@@ -2,15 +2,16 @@
 
 # paths
 DATA="$DIR/data"
-BIN="$DIR/src/bin"
+SRC="$DIR/src"
+BIN="$SRC/bin"
 USERS="$DATA/users"
 RUNTIME="$DATA/runtime"
 ROOMS="$DATA/rooms"
-SHELL_CONSTANTS="$DIR/src/constants/shell"
-NET_SERVER_DIR="$DIR/src/net/server"
+SHELL_CONSTANTS="$SRC/constants/shell"
+NET_SERVER_DIR="$SRC/net/server"
 SERVER_RUNTIME="$RUNTIME/server"
 CONNECTED_PLAYERS="$SERVER_RUNTIME/players"
-GAME="$DIR/src/game"
+GAME="$SRC/game"
 
 # paths relative to user room
 OBVIOUS_EXITS="./obvious_exits"
