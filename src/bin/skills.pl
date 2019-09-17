9646b2f7fa1cbc41b1f6a8b8e3149c33c78c54ba
Zachary Chandler <zachary.a.chandler96@gmail.com>
2019-09-17 13:17:59 -0500

    add skills command

---
+++ src/bin/skills.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib::model::skills;
+
+my $totalLevel = 0;
+
+print "\n";
+
+foreach my $skill (skills::all()) {
+    $totalLevel += @$skill[1];
+    print "@$skill[0]: level @$skill[1] + @$skill[2]xp\n";
+}
+
+print "Total level: ($totalLevel)\n";
+
--- src/lib/model/skills.pm
+++ src/lib/model/skills.pm
@@ -5,6 +5,16 @@ use strict;
 use warnings;
 
 use lib::model::commands;
+use environment::db;
+
+INIT {
+    $db::conn->do("CREATE TABLE IF NOT EXISTS skills (
+        user_name,
+        skill_name,
+        experience,
+        level
+    );");
+}
 
 sub exists {
     my $skill = shift;
@@ -19,6 +29,30 @@ sub exists {
 sub execute {
     my $skill = shift;
     commands::execute("bin/skills/$skill.pl", @_);
+}
+
+sub require {
+    my $skill = shift;
+    my $level = shift;
+
+    return 1 if ($db::conn->selectrow_array("select 1 from skills where user_name = ? and skill_name = ? and level >= ?", undef, $ENV{'USERNAME'}, $skill, $level));
+
+    print "Level $level $skill required\n";
+    return 0;
+}
+
+sub level {
+    my $skill = shift;
+    return $db::conn->selectrow_array("select level from skills where user_name = ? and skill_name = ?", undef, $ENV{'USERNAME'}, $skill);
+}
+
+sub experience {
+    my $skill = shift;
+    return $db::conn->selectrow_array("select experience from skills where user_name = ? and skill_name = ?", undef, $ENV{'USERNAME'}, $skill);
+}
+
+sub all {
+    return @{$db::conn->selectall_arrayref("select skill_name, level, experience from skills where user_name = ?", undef, $ENV{'USERNAME'})};
 }
 
 1;
