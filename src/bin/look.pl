f926687e3c9bfc890eb42b658a597b90010e59c8
Zachary Chandler <zachary.a.chandler96@gmail.com>
2019-10-05 19:54:05 -0500

    move minimap in look command

--- src/bin/look.pl
+++ src/bin/look.pl
@@ -8,6 +8,8 @@ use lib::model::links;
 use lib::model::entities::entity;
 use lib::model::entities::player;
 
+commands::runCommand("map", 2);
+
 my $room = player::getLocation($ENV{'USERNAME'});
 print room::name($room), "\n";
 print room::description($room), "\n";
@@ -19,7 +21,6 @@ print "  Obvious exits: @exits\n";
 my @ents = entity::getAll($room);
 
 if (@ents > 0) {
-    commands::runCommand("map", 2);
     print "  You can see:\n";
     print "    $_\n" for @ents;
 }
