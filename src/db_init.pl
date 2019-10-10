e249834f7e69c549f408d1fb1b9f90624b6f432a
Zachary Chandler <zachary.a.chandler96@gmail.com>
2019-10-10 14:59:04 -0500

    add src/db_init.pl script to reset database

---
+++ src/db_init.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Cwd;
+use File::Basename;
+use File::Find;
+use File::Spec;
+
+use lib dirname(Cwd::realpath($0));
+require environment::env;
+
+unlink "$ENV{'DIR'}/yggdrasil.db";
+
+sub init {
+    return unless ($_ =~ /.*\.pm/);
+
+    my $file = "$File::Find::dir/$_";
+    my $relPath = File::Spec->abs2rel($file, "$ENV{'DIR'}/src");
+    
+    require $relPath;
+}
+
+find (\&init, "$ENV{'DIR'}/src");
