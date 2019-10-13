3854b6a17d44470d2144c4a979cd92f9172d923b
Zachary Chandler <zachary.a.chandler96@gmail.com>
2019-10-13 11:17:41 -0500

    refactor look

--- src/bin/look.pl
+++ src/bin/look.pl
@@ -13,23 +13,25 @@ use lib::io::format;
 commands::runCommand("map", 2);
 
 my $room = player::getLocation($ENV{'USERNAME'});
-print room::name($room), "\n";
-print room::description($room), "\n";
+my $room_name = room::name($room);
+my $room_description = room::description($room);
 
 my @exits = links::getExits($room);
 @exits = ("none") unless (@exits > 0);
-print "  Obvious exits: @exits\n";
 
-my $ents = entity::getAllEx($room);
+my $ents = entity::getAllEx($room) // [];
+my @players = map {@$_[0]} grep { player::isPlayer(@$_[1]) } @$ents;
+my @ents = format::getCounts(map {@$_[0]} grep { ! player::isPlayer(@$_[1]) } @$ents);
 
-if (@$ents > 0) {
-    print "  You can see:\n";
-    for (@$ents) {
-        if (player::isPlayer(@$_[1])) {
-            print "    @$_[0]\n" 
-        } else {
-            my $item = format::withArticle(@$_[0]);
-            print "    $item\n" 
-        }
-    }
+my @look = (
+"$room_name\n",
+"$room_description\n",
+"  Obvious exits: @exits\n",
+"  There is: @players\n");
+
+if (@ents > 0) {
+    push @look, "  You can see:\n";
+    push @look, "    $_\n" for (@ents);
 }
+
+print @look;
