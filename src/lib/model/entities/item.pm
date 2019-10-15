10800ac9f94a81171f1c56d28e0b6a6a12cd8a1d
Zachary Chandler <zachary.a.chandler96@gmail.com>
2019-10-15 07:42:49 -0500

    merge stacks when moving items

--- src/lib/model/entities/item.pm
+++ src/lib/model/entities/item.pm
@@ -31,35 +31,63 @@ sub isStackable($) {
 
 sub getCount($) {
     my $entity_id = shift;
-    return $db::conn->seletrow_array('select count from item_instance where entity_id = ?', undef, $entity_id)
+    return $db::conn->selectrow_array('select count from item_instance where entity_id = ?', undef, $entity_id)
             // 1;
+}
+
+sub addCount($$) {
+    my $entity_id = shift;
+    my $count = shift;
+
+    $db::conn->do('insert or ignore into item_instance(entity_id, count) values (?, 1)', undef, $entity_id);
+    $db::conn->do('update item_instance set count = count + ? where entity_id = ?', undef, $count, $entity_id);
+}
+
+sub mergeCounts($$) {
+    my $entity_id1 = shift;
+    my $entity_id2 = shift;
+    my $add_count = getCount($entity_id2);
+
+    addCount($entity_id1, $add_count);
+    $db::conn->do('delete from item_instance where entity_id = ?', undef, $entity_id2);
+    entity::delete('item', $entity_id2);
+
+    return 1;
 }
 
 sub create($$) {
     my $name = shift;
     my $location = shift;
-    my $entity_id; 
-
+    
     if (isStackable $name) {
-        $entity_id = find($name, $location);
+        my $entity_id = find($name, $location);
 
         if (defined $entity_id) {
-            $db::conn->do('insert or ignore into item_instance(entity_id, count) values (?, 1)', undef, $entity_id);
-            $db::conn->do('update item_instance set count = count + 1 where entity_id = ?', undef, $entity_id);
+            addCount($entity_id, 1);
+            return $entity_id;
         }
     }
     
-    unless (defined $entity_id) {
-        $entity_id = entity::create($name, $location, 'item');
-    }
-
-    return $entity_id;
+    return entity::create($name, $location, 'item');;
 }
 
 sub setLocation($$$) {
     my $location = shift;
     my $name = shift;
     my $id = shift;
+    my $entity_id;
+
+    if (isStackable $name) {
+        $entity_id = find($name, $location);
+
+        if (defined $entity_id) {
+            my $temp_location = "p:$$";
+            my $moved = entity::setLocation($name, $temp_location, 'item', $id);
+            return 0 unless $moved;
+            return mergeCounts($entity_id, $id);
+        }
+    }
+
     return entity::setLocation($name, $location, 'item', $id);
 }
 
