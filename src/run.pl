d1c3b483828e3f90b6f66d12e36222a122b7167f
Zachary Chandler <zachary.a.chandler96@gmail.com>
2019-10-19 08:32:14 -0500

    add run command description

--- src/run.pl
+++ src/run.pl
@@ -4,12 +4,15 @@ use warnings;
 
 use Cwd;
 use File::Basename;
+use English qw(-no_match_vars);
 
 use lib dirname(Cwd::realpath($0));
 
 use environment::env;
 use lib::model::commands;
 
-die "usage: src/run.pl user command" unless @ARGV >= 1;
+die "usage: $PROGRAM_NAME user command\n  (runs a command as a user)\n" unless @ARGV >= 1;
 
 commands::runAs(@ARGV);
+
+1;
