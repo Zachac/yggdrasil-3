ddb22b2634dccf9a864d09bcbc7b8cf9eae4071e
Zachary Chandler <zachary.a.chandler96@gmail.com>
2019-11-08 12:23:49 -0600

    add option to create any entity_type to create.pl

--- src/bin/meta_scripts/create.pl
+++ src/bin/meta_scripts/create.pl
@@ -9,14 +9,22 @@ use lib::io::format;
 use lib::model::user::user;
 
 my $command = shift;
+my $item_type = $ARGV[0] =~ /^-/ ? substr(shift, 1) : 'item' if @ARGV > 0;
 
-die "usage: $command item name\n" unless @ARGV > 0;
+die "usage: $command [-entity_type] item name\n" unless @ARGV > 0;
 
 my $item_name = "@ARGV";
 my $location = player::getLocation($ENV{'USERNAME'});
 my $an_item_name = format::withArticle($item_name);
+my $result;
 
-die "could not create $item_name\n" unless item::create($item_name, $location);
+if ($item_type eq 'item') {
+    $result = item::create($item_name, $location);
+} else {
+    $result = entity::createByNameAndTypeAndLocation($item_name, $item_type, $location);
+}
+
+die "could not create $item_name\n" unless $result;
 
 user::echo "Created $an_item_name!\n";
 
